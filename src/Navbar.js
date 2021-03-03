8a5a6de5814dcfb5896dca1deb4edd8f3ddb6081
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-03 15:35:55 -0800

    Working on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,40 @@
 import React from "react";
 import logo from "./img/logo.svg";
 import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>;
+  const {
+    openSidebar,
+    openSubMenu,
+    closeSidebar,
+    closeSubMenu,
+  } = useGlobalContext();
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="Stripe Logo" />
+            <button className="btn toggle-btn" onClick={openSidebar}>
+              <FaBars />
+            </button>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <button className="link-btn">products</button>
+            </li>
+            <li>
+              <button className="link-btn">developers</button>
+            </li>
+            <li>
+              <button className="link-btn">company</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
