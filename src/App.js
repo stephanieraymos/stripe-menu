60e8fe58b7089611d68ade9059a74570fad49c09
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-03 14:54:33 -0800

    Context set up with state + functions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Submenu from "./Submenu";
 function App() {
   return (
     <>
-      <h2>Stripe Menu</h2>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
     </>
   );
 }
--- src/context.js
+++ src/context.js
@@ -1,2 +1,42 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
+
+export const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubMenu = () => {
+    setIsSubMenuOpen(true);
+  };
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubMenuOpen,
+        isSidebarOpen,
+        openSubMenu,
+        closeSubMenu,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
