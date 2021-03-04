209e03255431c784294229cc430b574db6f9061d
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-03 16:38:29 -0800

    Link displaying correctly on mouse over for each menu option

--- src/Submenu.js
+++ src/Submenu.js
@@ -22,6 +22,17 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
+      <div className={`submenu-center col-2`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
